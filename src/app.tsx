03814e1f8dedefe6cbf8d9bc9fe9b2872ef102bd
lihonglei <lihonglei2014@xiaochuankeji.cn>
2024-05-28 16:42:32 +0800

    init

---
+++ src/app.tsx
@@ -0,0 +1,20 @@
+// import { Suspense } from "react";
+// import ReactDOM from "react-dom/client";
+// import { RouterProvider } from "react-router-dom";
+// import router from "./router";
+
+// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+//   <Suspense>
+//     <Provider>
+//       <ConfigProvider
+//         locale={enUS}
+//         tablePagination={{
+//           sizeCanChange: true,
+//           sizeOptions: [10, 15, 20, 25, 30, 40, 50],
+//         }}
+//       >
+//         <RouterProvider router={router} />
+//       </ConfigProvider>
+//     </Provider>
+//   </Suspense>
+// );
